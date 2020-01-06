1a7ee548f5f6a532d3d2058d0b89bd9e5e036c82
Demyan Rogozhin <Demyan.Rogozhin@gmail.com>
2020-01-06 09:29:12 +0100

    upadte flake.nix to my nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   description = "Home Manager for Nix";
 
+  inputs.nixpkgs.url = "github:demyanrogozhin/nixpkgs/fork";
+
   outputs = { self, nixpkgs }: rec {
 
     nixosModules.home-manager = import ./nixos nixpkgs;
